4c4fc76b57fe9c0e6ad713be8fce4b7e3dc9d80d
sp-tam1m <white.hat.tamim@gmail.com>
2022-04-04 12:20:15 +0600

    something edit about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,6 +4,9 @@ const About = () => {
     return (
         <div className='mt-[100px]'>
             <h1 className='text-4xl text-center mt-5'>About Desktop</h1>
+            <p className='w-[500px] text-center mx-auto mt-[30px]'>A desktop computer is a personal computing device designed to fit on top of a typical office desk.
+
+                It houses the physical hardware that makes a computer run and connects to input devices such as the monitor, keyboard and mouse users interact with</p>
         </div>
     );
 };
